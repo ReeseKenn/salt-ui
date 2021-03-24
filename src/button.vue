6f5f395ce45ab82517545f5c80a9d5648a21b861
严宝瑞 <yanbaorui1997@163.com>
2021-03-24 16:31:13 +0800

    格式化代码

--- src/button.vue
+++ src/button.vue
@@ -12,13 +12,16 @@
         border-radius: var(--border-radius);
         border: 1px solid var(--border-color);
         background: var(--button-bg);
+
         &:hover {
             border-color: var(--border-color-hover);
             background-color: var(--button-hover-bg)
         }
+
         &:active {
             background-color: var(--button-active-bg);
         }
+
         &:focus {
             outline: none;
         }
