10808ae2300633ec08ab6d3a4b26357f9407c7be
严宝瑞 <yanbaorui@meibuyu.onaliyun.com>
2021-03-24 15:05:03 +0800

    使用单文件改写button组件

---
+++ src/button.vue
@@ -0,0 +1,25 @@
+<template>
+    <button class="s-button">按钮</button>
+</template>
+<script>
+    export default {}
+</script>
+<style lang="scss">
+    .s-button {
+        font-size: var(--font-size);
+        height: var(--button-height);
+        padding: 0 1em;
+        border-radius: var(--border-radius);
+        border: 1px solid var(--border-color);
+        background: var(--button-bg);
+        &:hover {
+            border-color: var(--border-color-hover);
+        }
+        &:active {
+            background-color: var(--button-active-bg);
+        }
+        &:focus {
+            outline: none;
+        }
+    }
+</style>
