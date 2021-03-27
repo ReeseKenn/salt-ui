2a303258e04cf4ecf8cf22877e199cd1726c5902
yanbaorui <yanbaorui1997@163.com>
2021-03-28 03:02:07 +0800

    添加button-group组件，并验证其子元素是否为button

---
+++ src/button-group.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="s-button-group">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "sButtonGroup",
+  mounted() {
+    console.log(this.$el.children)
+    for (let node of this.$el.children) {
+      let nodeName = node.nodeName.toLowerCase()
+      if (nodeName !== 'button') {
+        console.error(`'s-button-group''只允许以'button'为子元素'，'${nodeName}'不被允许插入`)
+      }
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.s-button-group {
+  display: inline-flex;
+  vertical-align: top;
+
+  > .s-button {
+    border-radius: 0;
+
+    &:nth-child(n+1) {
+      margin-right: -1px;
+    }
+
+    &:first-child {
+      border-top-left-radius: var(--border-radius);
+      border-bottom-left-radius: var(--border-radius);
+    }
+
+    &:last-child {
+      border-top-right-radius: var(--border-radius);
+      border-bottom-right-radius: var(--border-radius);
+    }
+
+    &:hover {
+      position: relative;
+      z-index: 1;
+    }
+  }
+}
+</style>
