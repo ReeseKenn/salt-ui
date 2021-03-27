b195981878dabee5933da9a52050e861be0476f4
yanbaorui <yanbaorui1997@163.com>
2021-03-27 23:58:00 +0800

    使用order属性更改icon位置

--- src/button.vue
+++ src/button.vue
@@ -1,31 +1,77 @@
 <template>
-    <button class="s-button">
+  <button class="s-button" :class="{[`icon-${iconPosition}`]:true}">
+    <svg class="icon" v-if="icon">
+      <use :xlink:href="`#i-${icon}`"></use>
+    </svg>
+    <div class="content">
       <slot></slot>
-    </button>
+    </div>
+  </button>
 </template>
 <script>
-    export default {}
+export default {
+  props: {
+    icon: {},
+    loading: {
+      type: Boolean,
+      default: false
+    },
+    iconPosition: {
+      type: String,
+      default: 'left',
+      validator(value) {
+        return value === 'left' || value === 'right'
+      }
+    }
+  }
+}
 </script>
 <style lang="scss">
-    .s-button {
-        font-size: var(--font-size);
-        height: var(--button-height);
-        padding: 0 1em;
-        border-radius: var(--border-radius);
-        border: 1px solid var(--border-color);
-        background: var(--button-bg);
+.s-button {
+  font-size: var(--font-size);
+  height: var(--button-height);
+  padding: 0 1em;
+  border-radius: var(--border-radius);
+  border: 1px solid var(--border-color);
+  background: var(--button-bg);
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
 
-        &:hover {
-            border-color: var(--border-color-hover);
-            background-color: var(--button-hover-bg)
-        }
+  &:hover {
+    border-color: var(--border-color-hover);
+    background-color: var(--button-hover-bg)
+  }
 
-        &:active {
-            background-color: var(--button-active-bg);
-        }
+  &:active {
+    background-color: var(--button-active-bg);
+  }
 
-        &:focus {
-            outline: none;
-        }
+  &:focus {
+    outline: none;
+  }
+
+  .icon {
+    width: 1em;
+    height: 1em;
+  }
+
+  > .icon {
+    order: 1;
+  }
+
+  > .content {
+    order: 2;
+  }
+
+  &.icon-right {
+    > .icon {
+      order: 2;
     }
+
+    > .content {
+      order: 1;
+    }
+  }
+}
 </style>
