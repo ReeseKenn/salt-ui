45f1f2db25ce5648b0a116b5d9231979ddc2930d
严宝瑞 <yanbaorui1997@163.com>
2021-03-29 18:26:03 +0800

    修改部分样式

--- src/button.vue
+++ src/button.vue
@@ -1,102 +1,104 @@
 <template>
-  <button class="s-button" :class="{[`icon-${iconPosition}`]:true}" @click="$emit('click')">
-    <s-icon class="icon" v-if="icon&&!loading" :name="icon"></s-icon>
-    <s-icon class="loading icon" v-if="loading" name="loading"></s-icon>
-    <div class="content">
-      <slot></slot>
-    </div>
-  </button>
+    <button class="s-button" :class="{[`icon-${iconPosition}`]:true}" @click="$emit('click')">
+        <s-icon class="icon" v-if="icon&&!loading" :name="icon"></s-icon>
+        <s-icon class="loading icon" v-if="loading" name="loading"></s-icon>
+        <div class="content">
+            <slot></slot>
+        </div>
+    </button>
 </template>
 <script>
-import Icon from './icon'
-export default {
-  components:{
-    'sIcon':Icon
-  },
-  props: {
-    icon: {},
-    loading: {
-      type: Boolean,
-      default: false
-    },
-    iconPosition: {
-      type: String,
-      default: 'left',
-      validator(value) {
-        return value === 'left' || value === 'right'
-      }
+    import Icon from './icon'
+
+    export default {
+        components: {
+            'sIcon': Icon
+        },
+        props: {
+            icon: {},
+            loading: {
+                type: Boolean,
+                default: false
+            },
+            iconPosition: {
+                type: String,
+                default: 'left',
+                validator(value) {
+                    return value === 'left' || value === 'right'
+                }
+            }
+        }
     }
-  }
-}
 </script>
 <style lang="scss">
-@keyframes spin {
-  0% {
-    transform: rotate(0deg);
-  }
-  100% {
-    transform: rotate(360deg);
-  }
-}
-
-.s-button {
-  color: #0a3d62;
-  font-size: var(--font-size);
-  height: var(--button-height);
-  padding: 0 1em;
-  border-radius: var(--border-radius);
-  border: 1px solid var(--border-color);
-  background: var(--button-bg);
-  display: inline-flex;
-  justify-content: center;
-  align-items: center;
-  vertical-align: top;
-  line-height: 1;
-
-  svg {
-    fill: #0a3d62;
-  }
-
-  &:hover {
-    border-color: var(--border-color-hover);
-    background-color: var(--button-hover-bg)
-  }
-
-  &:active {
-    background-color: var(--button-active-bg);
-  }
-
-  &:focus {
-    outline: none;
-  }
-
-  .icon {
-    width: 1em;
-    margin-right: 0.3em;
-  }
-
-  > .icon {
-    order: 1;
-  }
-
-  > .content {
-    order: 2;
-  }
-
-  &.icon-right {
-    > .icon {
-      order: 2;
-      margin-right: 0;
-      margin-left: 0.3em;
+    @keyframes spin {
+        0% {
+            transform: rotate(0deg);
+        }
+        100% {
+            transform: rotate(360deg);
+        }
     }
 
-    > .content {
-      order: 1;
-    }
-  }
+    .s-button {
+        color: #0a3d62;
+        font-size: var(--font-size);
+        height: var(--button-height);
+        padding: 0 1em;
+        border-radius: var(--border-radius);
+        border: 1px solid var(--border-color);
+        background: var(--button-bg);
+        display: inline-flex;
+        justify-content: center;
+        align-items: center;
+        vertical-align: top;
+        line-height: 1;
 
-  .loading {
-    animation: spin 2s infinite linear;
-  }
-}
+        svg {
+            fill: #0a3d62;
+        }
+
+        &:hover {
+            cursor: pointer;
+            border-color: var(--border-color-hover);
+            background-color: var(--button-hover-bg)
+        }
+
+        &:active {
+            background-color: var(--button-active-bg);
+        }
+
+        &:focus {
+            outline: none;
+        }
+
+        .icon {
+            width: 1em;
+            margin-right: 0.3em;
+        }
+
+        > .icon {
+            order: 1;
+        }
+
+        > .content {
+            order: 2;
+        }
+
+        &.icon-right {
+            > .icon {
+                order: 2;
+                margin-right: 0;
+                margin-left: 0.3em;
+            }
+
+            > .content {
+                order: 1;
+            }
+        }
+
+        .loading {
+            animation: spin 2s infinite linear;
+        }
+    }
 </style>
