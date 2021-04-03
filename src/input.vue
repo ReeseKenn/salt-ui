cbef12cfca884c41ca48b38563ef6418427c3c1b
yanbaorui <yanbaorui1997@163.com>
2021-04-03 11:33:54 +0800

    初始化input组件

---
+++ src/input.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="wrapper">
+    <input type="text">
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SInput"
+}
+</script>
+
+<style lang="scss" scoped>
+$border-color: #60a3bc;
+$border-color-focus: #3c6382;
+$height: 32px;
+
+.wrapper input {
+  height: $height;
+
+  &:focus {
+    border-color: $border-color-focus;
+    outline: none;
+  }
+}
+
+
+</style>
